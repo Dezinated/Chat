e4a482ce4673f9dd3edfe7451aad751d28035bef
Dezinated <dezinated@gmail.com>
2017-05-03 17:39:08 -0400

    Fixed 2 crashes Added firebase auth support Added anonymous auth Changed ID system to Firebase Auth

--- app/src/main/java/com/jason/chat/MainActivity.java
+++ app/src/main/java/com/jason/chat/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
@@ -14,6 +15,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.jason.chat.R;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -23,15 +29,20 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 
 import android.provider.Settings.Secure;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     DatabaseReference root;
+    private FirebaseAuth mAuth;
+
     String TAG = "MainActivity";
 
     private Button startBtn;
     private String android_id;
+
     Intent chatIntent;
     User me;
 
@@ -44,14 +55,43 @@ public class MainActivity extends AppCompatActivity {
 
         startBtn = (Button) findViewById(R.id.startChat);
 
-        root = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        signInAnonymously();
+
+
+        root = Utils.getDatabase().getReference();
         android_id = Secure.getString(this.getContentResolver(), Secure.ANDROID_ID);
-        me = new User(android_id);
+        me = new User(mAuth.getCurrentUser().getUid());
 
         //log on to app
         root.child("Users").child(me.id).setValue(me);
         root.child("Users").child(me.id).onDisconnect().removeValue();
 
+    }
+
+    private void signInAnonymously() {
+        // [START signin_anonymously]
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInAnonymously:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInAnonymously:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // [START_EXCLUDE]
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END signin_anonymously]
     }
 
     @Override
@@ -125,27 +165,7 @@ public class MainActivity extends AppCompatActivity {
 
         private void createNotitification(Context c){
 
-            NotificationManager mNotificationManager = (NotificationManager) c.getSystemService(Context.NOTIFICATION_SERVICE);
 
-            NotificationCompat.Builder mBuilder =
-                    new NotificationCompat.Builder(c)
-                            .setSmallIcon(R.drawable.ic_stat_name)
-                            .setContentTitle("Searching")
-                            .setContentText("Searching for someone to chat with");
-            Intent resultIntent = new Intent(c, MainActivity.class);
-
-            PendingIntent resultPendingIntent =
-                    PendingIntent.getActivity(
-                            c,
-                            0,
-                            resultIntent,
-                            PendingIntent.FLAG_UPDATE_CURRENT
-                    );
-
-            mBuilder.setContentIntent(resultPendingIntent);
-            mBuilder.setProgress(0, 0, true);
-// mId allows you to update the notification later on.
-            mNotificationManager.notify(1, mBuilder.build());
         }
 
         private void cancelNotification(Context c){
@@ -157,7 +177,7 @@ public class MainActivity extends AppCompatActivity {
         protected void onPreExecute() {
 
             Log.d(TAG, "start search");
-            createNotitification(MainActivity.this);
+
 
             queryRef = root.child("Users").orderByChild("avaliable");
             queryRef.addChildEventListener(new ChildEventListener() {
